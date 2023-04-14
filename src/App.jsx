37a1b87cb4014846e77816641c5d801d9b80c2a8
cyrilletual <cyrille.tual@gmail.com>
2023-04-14 16:52:42 +0200

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <React.Fragment>
+       <Header />
+       <Home />
+       <Footer />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home(){
+    return(
+        <main>
+            <section>
+                <h2>hi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ea itaque voluptatem ipsum quam iste et minus reiciendis nisi ipsam nihil velit dolorum, ut, repudiandae veniam qui tempore laboriosam cum debitis esse quis quo aliquam architecto. Facilis eveniet, inventore debitis voluptatibus vitae nemo, impedit porro delectus, harum temporibus provident repellat?</p>
+            </section>
+        </main>
+    )
+}
+
+export default Home
